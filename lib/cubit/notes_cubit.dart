7356072a0b30054cff6272805493f6fbba4650b2
Bon Aguilan <aguilanbon@gmail.com>
2024-10-29 09:19:09 +0800

    cubit getnotes, notes screen view

--- lib/cubit/notes_cubit.dart
+++ lib/cubit/notes_cubit.dart
@@ -1,10 +1,27 @@
+import 'dart:convert';
+
 import 'package:bloc/bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:http/http.dart';
 import 'package:note_taking_app/models/note.dart';
+import 'package:note_taking_app/services/notes_services.dart';
 
 part 'notes_state.dart';
 part 'notes_cubit.freezed.dart';
 
 class NotesCubit extends Cubit<NotesState> {
-  NotesCubit() : super(NotesState.initial());
+  final NotesServices _notesService;
+
+  NotesCubit(this._notesService) : super(const NotesState.initial());
+
+  Future<void> getNotes() async {
+    emit(const NotesState.loading());
+
+    try {
+      final notes = await _notesService.fetchNotes();
+      emit(NotesState.loaded(notes: notes));
+    } catch (e) {
+      emit(NotesState.error(message: e.toString()));
+    }
+  }
 }
--- lib/pages/note_list_screen.dart
+++ lib/pages/note_list_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:note_taking_app/cubit/notes_cubit.dart';
 
 class NotesListScreen extends StatelessWidget {
   const NotesListScreen({super.key});
@@ -6,17 +8,32 @@ class NotesListScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.pushNamed(context, '/add-edit-note');
-        },
-        child: const Icon(Icons.add),
-      ),
-      body: SafeArea(
-        child: Column(
-          children: [Text('sdsd')],
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.pushNamed(context, '/add-edit-note');
+          },
+          child: const Icon(Icons.add),
         ),
-      ),
-    );
+        body: SafeArea(
+          child: BlocBuilder<NotesCubit, NotesState>(
+            builder: (context, state) {
+              return state.when(
+                initial: () {
+                  context.read<NotesCubit>().getNotes();
+                  return const Center(
+                    child: CircularProgressIndicator.adaptive(),
+                  );
+                },
+                loading: () => const Center(
+                  child: CircularProgressIndicator.adaptive(),
+                ),
+                loaded: (notes) {
+                  return Text('loaded');
+                },
+                error: (message) => Text(message),
+              );
+            },
+          ),
+        ));
   }
 }
